391c6a3e06f1e221a68cbff42d7a9582b0b72a29
jordyhers <jordy925@gmail.com>
2021-05-17 23:21:26 +0300

    First Test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,38 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:boilerplate/features/boilerplate_app/domain/repositories/boiler_plate_repository.dart';
+import 'package:boilerplate/features/boilerplate_app/presentation/bloc/boiler_plate_bloc.dart';
+import 'package:boilerplate/features/boilerplate_app/presentation/pages/home_page.dart';
+import 'package:boilerplate/features/boilerplate_app/presentation/pages/tab_bar_items_page.dart';
+import 'package:boilerplate/features/boilerplate_app/presentation/widgets/cupertino_home_scaffold.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:boilerplate/main.dart';
+Future<void> pumpHomePage(WidgetTester tester) async {
+  await tester.pumpWidget(MaterialApp(
+    home: BlocProvider(
+      create: (context) => BoilerPlateBloc(BoilerPlateRep()),
+      child: HomePage(),
+    ),
+  ));
 
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+  await tester.pump();
+}
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+void main() async {
+  testWidgets('Test to display Home Page', (WidgetTester tester) async {
+    await pumpHomePage(tester);
+    expect(find.text('Home Page'), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  });
+
+  testWidgets('CHECK IF THE PAGE IS EMPTY', (WidgetTester tester) async {
+    await pumpHomePage(tester);
+    expect(find.text('Home Page'), findsOneWidget);
+
+
   });
 }
